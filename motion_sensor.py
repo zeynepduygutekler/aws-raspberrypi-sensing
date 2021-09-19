dd67d983bb87efb890dff0421fd812cdd1166a88
zeynepduygutekler <zeynepduygutekler17@gmail.com>
2021-09-20 01:11:59 +0800

    Columns fixed

--- motion_sensor.py
+++ motion_sensor.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 from datetime import datetime
-import datetime
 import boto3
 
 GPIO.setmode(GPIO.BCM)
@@ -9,33 +8,27 @@ pir_pin = 7
 GPIO.setup(pir_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
-class AWSDb(object):
+class AWSDb:
     def __init__(self, table_name='MotionData'):
         self.table_name = table_name
         self.db = boto3.resource('dynamodb')
         self.table = self.db.Table(table_name)
         self.client = boto3.client('dynamodb')
 
-    def put(self, data_entry):
-        self.table.put_item(
-            Item= data_entry
-        )
-
-    def motion_detection(self):
+    def motion_detection(self, pir_pin):
         timestamp = int(time.time())
         dateandtime = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
         location = "Adrian_Office"
 
-        data_entry = {"Timestamp": timestamp, "Date_Time": dateandtime, "Location": location, "Motion": 1}
-        print(data_entry)
-        self.put(data_entry)
+        data_entry = {"Timestamp": timestamp, "Date_Time": dateandtime, "Location": location, "Motion": 1, "Sensor_id": "AO_"+str(timestamp)}
+        self.table.put_item(Item=data_entry)
 
 
 if __name__ == "__main__":
 
     # Initialise the database object
     obj = AWSDb()
-    GPIO.add_event_detect(pir_pin, GPIO.BOTH, callback=AWSDb.motion_detection, bouncetime=10000)  # Frequency 10 sec
+    GPIO.add_event_detect(pir_pin, GPIO.BOTH, callback=obj.motion_detection, bouncetime=10000)  # Frequency 10 sec
 
     # To make the run active
     try:
